5288dea35062b684503a4dacf94c1f8d255f3de5
Gloria Morales <gmoraleslondono@gmail.com>
2020-07-05 14:24:02 +0200

    Add home page

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
