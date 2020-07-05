fe1b0a70f7e7d6875faf7fd896e599dd7286109a
Gloria Morales <gmoraleslondono@gmail.com>
2020-07-05 14:05:48 +0200

    Add vue configuration

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+
+Vue.config.productionTip = false
+
+Vue.use(Vuex, VueRouter)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
