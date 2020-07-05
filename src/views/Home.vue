cff3d59ee4bd3b9d2c8756c1633cdd8f667598cd
Gloria Morales <gmoraleslondono@gmail.com>
2020-07-05 15:50:49 +0200

    Fix home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="hello">
-    <h1>{{ title }}</h1>
+  <div class="home">
+    <h1>Let's play</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
   props: {
     title: String
   }
