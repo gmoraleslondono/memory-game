cf0195bc4cb3734debdaf5ca819ab2557703166a
Gloria Morales <gmoraleslondono@gmail.com>
2020-07-05 14:08:32 +0200

    Add instructions view

---
+++ src/views/Instructions.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="instructions">
+  <h1>Rules</h1>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Instructions',
+    data() {
+        return {
+
+        };
+    }
+};
+</script>
+
+<style>
+
+</style>
