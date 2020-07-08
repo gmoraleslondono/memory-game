6627d78b5729dc69099181536d113469748b77ea
Gloria Morales <gmoraleslondono@gmail.com>
2020-07-08 23:29:33 +0200

    Add top board info

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,8 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    stars: 3,
+    numMoves: 0
   },
   mutations: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="home">
     <h1>Let's play</h1>
+      <main class="container">
+        <section>
+          <button class="restart" style="background-color: orange">
+              <i class="fa fa-repeat"></i>
+              Reset
+          </button>
+          <div>
+              <ul class="stars">
+              <li v-for="(star, index) in stars" :key="index" class="star">
+                  <i class="${index} fa fa-star"></i>
+              </li>
+              </ul>
+              <p class="moves">Moves: {{ numMoves }}</p>
+          </div>
+        </section>
+    </main>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: 'Home',
+  computed: {
+    ...mapState ([
+      "stars",
+      "numMoves",
+    ])
+  }
 }
 </script>
 
@@ -21,5 +45,44 @@ export default {
 }
 .home h1 {
   text-align: center;
+}
+.gameController .stars {
+  padding: 0px;
+  display: inline-block;
+  margin: 2em auto 0;
+}
+
+.star {
+  list-style: none;
+  display: inline-block;
+  margin: 0 0.2em;
+  font-size: 1.5em;
+  color: $orange;
+}
+.moves {
+  font-weight: bold;
+}
+
+.gameController .restart {
+  float: right;
+  cursor: pointer;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 1em;
+}
+
+.restart {
+  background-color: $orange;
+  padding: 10px 20px;
+  border-radius: 20px;
+}
+
+.fa-repeat {
+  background-color: $orange;
 }
 </style>
