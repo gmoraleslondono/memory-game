7319a0400595bbe64883fa193d1ad1abd0dac4eb
Gloria Morales <gmoraleslondono@gmail.com>
2020-07-23 08:24:06 +0200

    Add function to shuffle cards randomly

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -48,7 +48,31 @@ export default {
       "cardsMatched",
     ]),
     ...mapGetters(["deck"])
-  }
+  },
+  methods: {
+    shuffle(cards) {
+      this.deck.cards = [];
+      var currentIndex = cards.length,
+      temporaryValue,
+      randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = cards[currentIndex];
+        cards[currentIndex] = cards[randomIndex];
+        cards[randomIndex] = temporaryValue;
+      }
+
+      this.deck.cards = cards;
+    }
+  },
+  created() {
+    this.shuffle(this.deck.cards);
+  },
 }
 </script>
 
